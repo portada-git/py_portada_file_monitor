b1a46365dd76aa53aef3925e1279d8bbc0bc6e0a
jcanell4 <jcanell4@xtec.cat>
2026-01-28 10:22:52 +0100

    rename package to improve import

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='py_portada_file_monitor',
-    version='0.0.2',
+    version='0.0.3',
     description='....... for PortADa project',
     author='PortADa team',
     author_email='jcbportada@gmail.com',
     license='MIT',
     url="https://github.com/portada-git/py_portada_file_monitor.git",
-    packages=['monitor'],
+    packages=['portada_file_monitor'],
     py_modules=['file_event_handler'],
     install_requires=[
         "watchdog",
