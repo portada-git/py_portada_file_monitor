822c723554cf0cebcf144814ba425d8caa7024fc
jcanell4 <jcanell4@xtec.cat>
2026-02-03 00:11:56 +0100

    version 0.1.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='py_portada_file_monitor',
-    version='0.1.6',
+    version='0.1.7',
     description='....... for PortADa project',
     author='PortADa team',
     author_email='jcbportada@gmail.com',
